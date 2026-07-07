b8ecf31dd656c918169b237306cec9bc902110bd
Tori <tkhandaker3@gmail.com>
2026-07-07 03:25:47 -0400

    added working test function which creates a new student and adds them to the database

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,82 @@ require('dotenv').config();
 
 const express = require('express');
 const SQL = require('mysql2/promise');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const connectionPool = SQL.createPool(
     {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        waitForConnections = true,
-        connectionLimit = 100,
-        queueLimit = 0
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'user',
+        password: process.env.DB_PASSWORD || 'password',
+        database: process.env.DB_NAME || 'lms',
+        waitForConnections: true,
+        connectionLimit: 100,
+        queueLimit: 0
     }
 );
 
 const port = process.env.port || 3000;
+
+app.post('/api/students', 
+    async (req, res) => {
+        const { name, email, password } = req.body;
+
+        if (!name || !name.trim()) {
+            return res.status(400).json({ error: "No name given." });
+        }
+        if (!email || !email.trim()) {
+            return res.status(400).json({ error: "No email given." });
+        }
+        if (!password || !password.trim()) {
+            return res.status(400).json({ error: "No password given."})
+        }
+        
+        const cleanName = name.trim();
+        const cleanEmail = email.trim().toLowerCase();
+        const cleanPswd = password.trim();
+
+        if (!cleanEmail.includes('@') || !cleanEmail.includes('.')) {
+            return res.status(400).json({ error: "Email is not valid. "});
+        }
+        if (!cleanPswd.length > 8) {
+            return res.status(400).json({ error: "Password is too short. Must be more than 8 characters long."});
+        }
+
+        const [alreadyExistingEmail] = await connectionPool.query(
+            'SELECT id FROM students WHERE email = ?', [cleanEmail]
+        );
+        if (alreadyExistingEmail.length > 0) {
+            return res.status(409).json({ error: "Email already exists."});
+        }
+
+        try { // has to be hashed--will add later
+            const [successfulInsertion] = await connectionPool.query(
+                'INSERT INTO students (name, email, password, created_at) VALUES (?, ?, ?, NOW())',
+                [cleanName, cleanEmail, cleanPswd]
+            );
+            res.status(201).json({ 
+                message: "Student " + cleanName + " created", 
+                student: { 
+                    id: successfulInsertion.insertId,
+                    name: cleanName,
+                    email: cleanEmail
+                } }); 
+        }
+        catch(error) { 
+            console.error('Database error', error);
+            res.status(500).json( { error: "Database error." });
+        }
+    }
+);
+
+
+app.listen(port, 
+    () => {
+        console.log("Backend server running at http://localhost:${port}");
+    }
+);
 
--- backend/studentServices.js
+++ backend/studentServices.js
@@ -1,10 +1,30 @@
 const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
-
+const API_URL = process.env.API_URL || "http://localhost:3000/api"
 // messenger functions to encapsulate database accesses in server.js
 // below are example functions
 
-export async function createStudent(newStudentID) {
+export async function createStudent(name, email, password) {
+    try {
+        const res = await fetch("${API_URL}/students", 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({name, email, password})
+            });
+            const data = await res.json();
 
+            if (!res.ok) {
+                throw new Error(data.error);
+            }
+            
+            return data;
+    }   
+    catch {
+        console.error("Error adding student");
+        return null;
+    }
 }
 
 export async function getAssignmentForStudent(assignmentID) {
